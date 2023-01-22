88f3952f2c3f3e2c8b68718b1892f38fc51d0490
KirillovMikhail <kirillovmikhail88@gmail.com>
2023-01-23 00:52:48 +0300

    add_UI

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@
   // import 'swiper/css/navigation';
   // import 'swiper/css/pagination';
 
-
 const swiper = new Swiper('.education__swipper', {
   // modules: [Navigation, Pagination],
 
@@ -42,7 +41,7 @@ acc.open(0)
 const swiperModal = new Swiper('.modal__swiper', {
   // modules: [Navigation, Pagination],
 
-  loop: true,
+  // loop: true,
 
   // If we need pagination
   pagination: {
@@ -56,3 +55,59 @@ const swiperModal = new Swiper('.modal__swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const advBtn = document.querySelectorAll('.advantages__buttom-item');
+const modalWin = document.querySelector('.modal');
+const modalSwiperDom = document.querySelector('.modal__swiper');
+const modalNextSlide = document.querySelector('.modal__swiper-next')
+
+advBtn.forEach((el)=>{
+  el.addEventListener('click', ()=>{
+    modalWin.classList.add('vissual');
+    modalSwiperDom.classList.add('vissual');
+    let slide = getSlide(el)
+    console.log(slide)
+    changeSlide(swiperModal, slide)
+  })
+})
+
+function getSlide(button) {
+  return Number(button.dataset.slide)
+}
+
+function changeSlide (swiper, slide) {
+  swiper.slideTo(slide-1)
+
+}
+
+modalSwiperDom.addEventListener('click', event => {
+  event._isClickWithinModal = true;
+});
+
+modalWin.addEventListener('click', event => {
+  if (event._isClickWithinModal) return;
+  modalWin.classList.remove('vissual')
+  modalSwiperDom.classList.remove('vissual')
+})
+
+
+
+const anchors = document.querySelectorAll('a[href*="#"]') // links
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1)
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+
+
+
+
+
